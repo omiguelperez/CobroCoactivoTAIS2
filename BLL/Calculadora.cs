f8b24a87704ae3fe76082a559dfe9c7d8aca4bf3
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-04-18 19:15:30 -0500

    Se agrega el proyecto para BDD
    En el proyecto hay un ejemplo probando la funcionalidad de una calculadora básica.

---
+++ BLL/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Calculadora
+    {
+        public int PrimerNumero { get; set; }
+        public int SegundoNumero { get; set; }
+        public int Resultado { get; set; }
+
+        public void Sumar()
+        {
+            Resultado = PrimerNumero + SegundoNumero;
+        }
+    }
+}
