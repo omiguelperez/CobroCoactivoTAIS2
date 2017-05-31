7ae969b656dabe1bddf643ebf7850d4596af2aa7
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-05-31 07:35:18 -0500

    Debo hacer commit (probando algo)

---
+++ BLLTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace BLLTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestAddTwoNumber()
+        {
+            Assert.AreEqual(1 + 1, 2);
+        }
+    }
+}
